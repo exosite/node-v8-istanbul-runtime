7ccc2a980a84f9b338d0eb94e6491d38a8662bab
Renaud Sauvain <renaudsauvain@exosite.com>
2019-12-24 17:29:42 +0800

    Adding test message

--- test.js
+++ test.js
@@ -6,30 +6,29 @@ const coverage = new Coverage();
 async function test () {
   try {
     await coverage.get();
-    assert('Should not be here' === false);
+    assert(false, 'Should not get without start');
   } catch (e) {
-    assert(e.message === 'Session is not connected', e.message);
+    assert.strictEqual(e.message, 'Session is not connected', 'Should not get without start');
   }
   await coverage.start();
   const report = await coverage.get();
-  assert(typeof report['/index.js'] === 'object');
+  assert.strictEqual(typeof report['/index.js'], 'object', 'Has index.js');
 
   const summary = await coverage.summarize();
-  assert(typeof summary.summary.lines === 'object');
-  assert(typeof summary.summary.statements === 'object');
-  assert(typeof summary.summary.functions === 'object');
-  assert(typeof summary.summary.branches === 'object');
-  assert(typeof summary['/index.js'] === 'object');
-  assert(typeof summary['/index.js'].lines === 'object');
-  assert(typeof summary['/index.js'].statements === 'object');
-  assert(typeof summary['/index.js'].functions === 'object');
-  assert(typeof summary['/index.js'].branches === 'object');
+  assert.strictEqual(typeof summary.summary, 'object', 'Has summary');
+  assert.strictEqual(typeof summary.summary.lines, 'object', 'Has summary.lines');
+  assert.strictEqual(typeof summary.summary.statements, 'object', 'Has summary.statements');
+  assert.strictEqual(typeof summary.summary.functions, 'object', 'Has summary.functions');
+  assert.strictEqual(typeof summary.summary.branches, 'object', 'Has summary.branches');
+  assert.strictEqual(typeof summary['/index.js'].lines, 'object', 'Has /index.js.lines');
+  assert.strictEqual(typeof summary['/index.js'].statements, 'object', 'Has /index.js.statements');
+  assert.strictEqual(typeof summary['/index.js'].functions, 'object', 'Has /index.js.functions');
+  assert.strictEqual(typeof summary['/index.js'].branches, 'object', 'Has /index.js.branches');
 
   const full = await coverage.summarize(report, true);
-  assert(typeof full['/index.js'] === 'object');
-  assert(typeof full['/index.js'].lines === 'object');
-  assert(typeof full['/index.js'].statementMap === 'object');
-  assert(typeof full['/index.js'].lines === 'object');
+  assert.strictEqual(typeof full['/index.js'], 'object', 'Has index.js');
+  assert.strictEqual(typeof full['/index.js'].lines, 'object', 'Has /index.js.lines');
+  assert.strictEqual(typeof full['/index.js'].statementMap, 'object', 'Has /index.js.statementMap');
 
   await coverage.stop();
   await coverage.start();
